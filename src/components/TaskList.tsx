a5ecaeceaa2bbc094e81741ae36c92997cb5c878
Cauan Teixeira <cauantt007@gmail.com>
2023-01-28 20:11:58 -0300

    style textarea

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -69,6 +69,7 @@ export function TaskList() {
                     placeholder='Adicione uma nova tarefa'
                     value={newTaskText}
                     onChange={handleNewTaskChange}
+                    required
                 
                 />
                 
